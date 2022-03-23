e182254144286906a295c3d01a6f421dc6e57e9d
Junyoung Heo <jyheo0@gmail.com>
2022-03-23 22:55:01 +0900

    add exception message

--- examples/appium-test/week1_test.py
+++ examples/appium-test/week1_test.py
@@ -2,44 +2,52 @@ from androguard.core.bytecodes.apk import APK
 from appium import webdriver
 from appium.webdriver.common.appiumby import AppiumBy
 
-# 테스트할 APK 파일의 위치
-APP_LOCATION = r'C:\Users\jyheo\AndroidStudioProjects\MyApplication\app\build\intermediates\apk\debug\app-debug.apk'
-
-ANDROID_BASE_CAPS = {
-    'app': APP_LOCATION,
-    'automationName': 'UIAutomator2',
-    'platformName': 'Android',
-    'platformVersion': '12.0',  # 오류 발생시 본인의 안드로이드 버전으로 바꿔야 합니다.
-    'deviceName': 'Android Emulator',
-    'allowTestPackages': 'true'
-}
-
-EXECUTOR = 'http://127.0.0.1:4723/wd/hub'
-
 
 class CheckHW():
     PACKAGE = 'com.example.myapplication'
     MAIN_ACTIVITY = '.MainActivity'
+    EXECUTOR = 'http://127.0.0.1:4723/wd/hub'
+    ANDROID_BASE_CAPS = {
+        'app': 'app-debug.apk',  # path to the app package
+        'automationName': 'UIAutomator2',
+        'platformName': 'Android',
+        'platformVersion': '12.0',  # platform version of emulator or device where app will be tested
+        'deviceName': 'Android Emulator',
+        'allowTestPackages': 'true',  # add -t flags to adb command when install the app package
+        'enforceAppInstall' : 'true'
+    }
 
-    def __init__(self):
-        apk = APK(APP_LOCATION)
+    def __init__(self, appLocation, platformVersion = '12.0'):
+        apk = APK(appLocation)
         self.PACKAGE = apk.get_package()
 
+        self.ANDROID_BASE_CAPS['app'] = appLocation
+        self.ANDROID_BASE_CAPS['platformVersion'] = platformVersion
         self.driver = webdriver.Remote(
-            command_executor=EXECUTOR,
-            desired_capabilities=ANDROID_BASE_CAPS
+            command_executor=self.EXECUTOR,
+            desired_capabilities=self.ANDROID_BASE_CAPS
         )
         self.driver.implicitly_wait(10)
 
-    def __del__(self):
-        self.driver.remove_app(self.PACKAGE)  # remove app
-        self.driver.quit()
 
     def test_myapp(self):
-        txtview = self.driver.find_element(AppiumBy.ID, 'txtView')
-        return txtview.text == 'Hello World!'
+        try:
+            txtview = self.driver.find_element(AppiumBy.ID, 'txtView')
+            if txtview.text == 'Hello World!':
+                return 'OK'
+            else:
+                return 'Incorrect Text'
+        except:
+            return 'No txtView ID'
 
 
-chw = CheckHW()
-r = chw.test_myapp()
-print(r)
+
+if __name__ == '__main__':
+    # 테스트할 APK 파일의 위치
+    DEF_APP_LOCATION = r'C:\Users\jyheo\AndroidStudioProjects\MyApplication\app\build\intermediates\apk\debug\app-debug.apk'
+    ANDROID_VERSION = '12.0'
+
+    chw = CheckHW(DEF_APP_LOCATION, ANDROID_VERSION)
+    r = chw.test_myapp()
+    print(r)
+
