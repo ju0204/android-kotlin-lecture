3c727b4809509a8f6d9796409366c7f85609a16e
Junyoung Heo <jyheo0@gmail.com>
2022-10-28 00:03:26 +0900

    remote config, storage lab.

--- examples/firebasetest/app/src/main/java/com/example/firebasetest/RemoteConfigActivity.kt
+++ examples/firebasetest/app/src/main/java/com/example/firebasetest/RemoteConfigActivity.kt
@@ -1,19 +1,32 @@
 package com.example.firebasetest
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import com.example.firebasetest.databinding.ActivityRemoteConfigBinding
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.remoteconfig.ktx.remoteConfig
 import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.ktx.storage
 
 class RemoteConfigActivity : AppCompatActivity() {
+    companion object {
+        const val imgSpringURL = "gs://android-kotlin-lecture.appspot.com/season/spring.jpg"
+        const val imgSummerURL = "gs://android-kotlin-lecture.appspot.com/season/summer.jpg"
+        const val imgFallURL = "gs://android-kotlin-lecture.appspot.com/season/fall.jpg"
+        const val imgWinterURL = "gs://android-kotlin-lecture.appspot.com/season/winter.jpg"
+    }
+
+    private val remoteConfig = Firebase.remoteConfig
+    private lateinit var binding: ActivityRemoteConfigBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityRemoteConfigBinding.inflate(layoutInflater)
+        binding = ActivityRemoteConfigBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val remoteConfig = Firebase.remoteConfig
         val configSettings = remoteConfigSettings {
             minimumFetchIntervalInSeconds = 1 // For test purpose only, 3600 seconds for production
         }
@@ -28,6 +41,37 @@ class RemoteConfigActivity : AppCompatActivity() {
                     binding.textYourPrice.text = yourPrice.toString()
                     binding.textCheatEnabled.text = cheatEnabled.toString()
                 }
+        }
+
+        refreshImage()
+
+        binding.refresh.setOnClickListener {
+            refreshImage()
+        }
+    }
+
+    private fun refreshImage() {
+        remoteConfig.fetchAndActivate()
+            .addOnCompleteListener(this) {
+                if (it.isSuccessful) {
+                    val season = remoteConfig.getString("season")
+                    val imgRef = when (season) {
+                        "spring" -> Firebase.storage.getReferenceFromUrl(imgSpringURL)
+                        "summer" -> Firebase.storage.getReferenceFromUrl(imgSummerURL)
+                        "fall" -> Firebase.storage.getReferenceFromUrl(imgFallURL)
+                        else -> Firebase.storage.getReferenceFromUrl(imgWinterURL)
+                    }
+                    displayImageRef(imgRef, binding.imageView)
+                }
+            }
+    }
+
+    private fun displayImageRef(imageRef: StorageReference?, view: ImageView) {
+        imageRef?.getBytes(Long.MAX_VALUE)?.addOnSuccessListener {
+            val bmp = BitmapFactory.decodeByteArray(it, 0, it.size)
+            view.setImageBitmap(bmp)
+        }?.addOnFailureListener {
+            // Failed to download the image
         }
     }
 }
